c2574b85a1d6766e70262f88f949851ebbe12984
Maro_Medhat <marwanmedhat.itech@gmail.com>
2024-06-15 03:04:51 +0300

    test

--- Src/index.js
+++ Src/index.js
@@ -5,16 +5,17 @@ let number = Math.floor(Math.random() * max) + min;
 
 let img = document.getElementById("img");
 
-img.src = `/Img/dice(${number}).png`;
+img.src = `Img/dice(${number}).png`;
 
 let btn = document.getElementById("btn");
+
 let i = 0;
 let temp = number;
 
 function rollDice() {
     if (i < 10) {
         number = Math.floor(Math.random() * max) + min;
-        img.src = `/Img/dice(${number}).png`;
+        img.src = `Img/dice(${number}).png`;
         
         if (number !== temp) {
             i++;
